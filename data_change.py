deac0cedac9515df432d5ec31c9edda25dd5d63a
ltg2030 <ltg2030@daum.net>
2016-11-09 16:36:59 +0900

    final
    소스 및 보고서

--- data_change.py
+++ data_change.py
@@ -4,15 +4,16 @@ with pydivert.WinDivert("tcp and tcp.PayloadLength > 0") as w:
     
     for packet in w:
         
-        if packet.is_inbound == True:
-            origin_payload = packet.tcp.payload
-            modify_payload = origin_payload.replace(b'Michael', b'Gilbert')
-            packet.tcp.payload = data
-            
-        if packet.is_outbound == True:
+        if packet.is_inbound:
+            origin_data = packet.tcp.payload
+            modify_data = origin_data.replace('Michael', 'Gilbert')
+            packet.tcp.payload = modify_data
+        if packet.is_outbound:
             data = packet.tcp.payload
-            print datar
-            data = data.replace(b'Accept-Encoding: gzip', b'Accept-Encoding:     ')
-            packet.tcp.payload = data
+            if data.find('\x0d\x0a\x0d\x0a') != -1:
+                head = data.split('\x0d\x0a\x0d\x0a')[0]
+                body = data.split('\x0d\x0a\x0d\x0a')[1]
+                head = head.replace('Accept-Encoding: gzip', 'Accept-Encoding:     ')
+                packet.tcp.payload = head+'\x0d\x0a\x0d\x0a'+body
             
         w.send(packet, recalculate_checksum=True)
